a07f7c4e8ee493f80e9a9c753b6d28af262f9dcc
Susan Yuen <susan_loves_cheese@hotmail.com>
2016-09-21 11:04:47 -0400

    updating files

--- app/src/main/java/com/example/susan/acnetracker/MainActivity.java
+++ app/src/main/java/com/example/susan/acnetracker/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.susan.acnetracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
 
-public class MainActivity extends AppCompatActivity {
+import layout.CalendarFragment;
+
+public class MainActivity extends AppCompatActivity implements
+        CalendarFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
